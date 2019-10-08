9765ebfc48daca6f1753b79a6c627c53055134cb
Lukas Panni <lukas-panni@web.de>
2019-10-08 16:07:19 +0200

    Webengineering_II; Authentication Test

---
+++ Webengineering_II/Rechner_Auth/src/servlets/Validate.java
@@ -0,0 +1,55 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import beans.CalculatorBean;
+
+/**
+ * Servlet implementation class Validate
+ */
+@WebServlet("/Validate")
+public class Validate extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Validate() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		CalculatorBean bean = (CalculatorBean) request.getSession().getAttribute("calculatorBean");
+		if (bean.getOK()) {
+			request.getServletContext().getRequestDispatcher("/Result.jsp").forward(request, response);
+		} else {
+			request.getServletContext().getRequestDispatcher("/Form.jsp").forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
