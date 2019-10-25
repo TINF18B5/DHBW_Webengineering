9a379c04d8c4e965c0e8bfd8be7f1a499a2a1379
Lukas Panni <lukas-panni@web.de>
2019-10-25 10:05:36 +0200

    Jasmine Tests

---
+++ Webengineering_II/Testing/Calculator/CalculatorSpec.js
@@ -0,0 +1,18 @@
+describe("Calculator", function() {
+    var calculator;
+    beforeEach(function() {
+        calculator = new Calculator(3, 4);
+    });
+    it("sum7", function() {
+        expect(calculator.sum()).toEqual(7);
+    });
+    it("diff-1", function() {
+        expect(calculator.diff()).toEqual(-1);
+    });
+    it("sumNot0", function() {
+        expect(calculator.sum()).not.toEqual(0);
+    });
+    it("diffNot0", function() {
+        expect(calculator.diff()).not.toEqual(0);
+    });
+});
