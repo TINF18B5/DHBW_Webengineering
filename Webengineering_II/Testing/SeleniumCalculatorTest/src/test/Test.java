97a58c708fa549465d27293cfaa06011717449af
Lukas Panni <lukas-panni@web.de>
2019-11-08 08:50:01 +0100

    Selenium Test

--- Webengineering_II/Testing/SeleniumCalculatorTest/src/test/Test.java
+++ Webengineering_II/Testing/SeleniumCalculatorTest/src/test/Test.java
@@ -7,8 +7,9 @@ import junit.framework.TestCase;
 
 public class Test extends TestCase {
 	public void test() {
+		System.setProperty("webdriver.gecko.driver", "/usr/bin/geckodriver");
 		WebDriver driver = new FirefoxDriver();
-		driver.get("localhost/Rechner_Auth/Form.jsp");
+		driver.get("localhost:8080/Rechner_Auth/Form.jsp");
 
 		driver.close();
 	}
